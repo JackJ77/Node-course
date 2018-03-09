76993da3282df499a26277b42c49c415b5996699
JackJ77 <ku56@hotmail.com>
2018-03-09 16:46:03 +0100

    Add css for chat app

--- server/server.js
+++ server/server.js
@@ -18,15 +18,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New connection');
 
-  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
-
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
   socket.on('createMessage', (message, callback) => {
     console.log('createMessage ', message);
 
     io.emit('newMessage', generateMessage(message.from, message.text));
-    callback('This is from server');
+    callback();
     //all recive message but not you
     // socket.broadcast.emit('newMessage', {
     //   from: message.from,
