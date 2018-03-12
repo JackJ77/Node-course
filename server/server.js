9b94d0a6d168ee96b6ce62627ad1f9a30daa31b4
JackJ77 <ku56@hotmail.com>
2018-03-12 15:12:35 +0100

    isolate rooms massages

--- server/server.js
+++ server/server.js
@@ -37,9 +37,12 @@ io.on('connection', (socket) => {
   });
 
   socket.on('createMessage', (message, callback) => {
-    console.log('createMessage ', message);
+    const user = users.getUser(socket.id);
 
-    io.emit('newMessage', generateMessage(message.from, message.text));
+    if (user && isRealString(message.text)) {
+      io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+    }
+
     callback();
     //all recive message but not you
     // socket.broadcast.emit('newMessage', {
@@ -50,7 +53,11 @@ io.on('connection', (socket) => {
   });
 
   socket.on('createLocationMessage', (coords) => {
-    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude))
+    const user = users.getUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+    }
   });
 
   socket.on('disconnect', () => {
